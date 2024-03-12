c8420ab59077b2ed11312367f114be53bc4ef251
Marco Antonio <dev.10@pbhub.com.br>
2024-03-12 13:50:02 -0300

    calculating calculating quantities

--- App.js
+++ App.js
@@ -1,30 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button, Pressable } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, Button, SafeAreaView, Alert, Keyboard } from 'react-native';
 
 export default function App(props) {
-    const {onPress, title = 'Calcular!'} = props
+    
+    const [vbb, onChangeVbb] = React.useState("");
+    const [rb, onChangeRb] = React.useState("");
+    const [bcc, onChangeBcc] = React.useState("");
+    const [rc, onChangeRc] = React.useState("");
+    const [vcc, onChangeVcc] = React.useState("");
+
+    const current = (v, r) => {
+        const vbe = 0.07
+        
+        return (v - vbe) / r
+    }
+
+    const power = (v, i) => {
+        return v * i
+    }
+
+    const calculate = () => {
+        console.log(vbb, typeof(vbb), typeof(rb))
+
+        const ib    = current(vbb, rb) // Corrente IB
+        const ic    = ib * Number(bcc) // Corrente IC
+        const vce   = Number(vcc) - (Number(rc) * ic) // Tensão Vce
+        const pd    = power(vce, ic) // Potência dissipada no transistor PD
+
+
+        const str = "Corrente Ib: " + ib.toFixed(6) + "\nCorrente Ic: " + ic.toFixed(6) + "\nTensão Vce: " + vce.toFixed(6)  + "\nPotência dissipada no transistor Pd: " + pd.toFixed(6)
+
+        Alert.alert(str)
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Calculadora Eletronica I</Text>
-            <View style={styles.inputContainer}>
-                    <TextInput 
+        <SafeAreaView style={styles.container} >
+            <View style={styles.container} onPress={(event) => {event.stopPropagation(); Keyboard.dismiss();}}>
+                <Text style={styles.title}>Calculadora Eletronica I</Text>
+                <View style={styles.inputContainer}>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="V_bb"
+                            onChangeText={onChangeVbb}
+                            value={vbb}
+                        />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
                         style={styles.input}
-                        placeholder="Campo1"
+                        placeholder="R_b"
+                        value={rb}
+                        onChangeText={onChangeRb}
+                        
                     />
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Campo2"
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="B_cc"
+                        value={bcc}
+                        onChangeText={onChangeBcc}
+                        
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="R_c"
+                        value={rc}
+                        onChangeText={onChangeRc}
+                        
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={onChangeVcc}
+                        value={vcc}
+                        placeholder="V_cc"
+                        
+                    />
+                </View>
+                
+                <Button 
+                    title="Calcular!" 
+                    color="#ff00ff"
+                    onPress={calculate}
                 />
             </View>
-            
-            <Pressable style={styles.button} onPress={onPress}>
-                <Text style={styles.text}>{title}</Text>
-            </Pressable>
-            
-            <Button title="Calcular!" color="#ff00ff"/>
-        </View>
+        </SafeAreaView>
     );
 }
 
@@ -41,7 +105,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   inputContainer: {
-    width: 300,
+    width: 150,
     height: 40,
     borderRadius: 10,
     borderColor: "#0EDF4A",
